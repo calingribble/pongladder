a09fc8ad71af88f03791eec4739decbe66cc3cec
Calin Gribble <dev+calin@blinqmedia.com>
2014-09-20 16:29:31 -0400

    money stand like 8 ft just like 2 midgets

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function (grunt) {
     // Watches files for changes and runs tasks based on the changed files
     watch: {
       bower: {
-        files: ['bower.json'],
+        files: ['./bower.json'],
         tasks: ['wiredep']
       },
       js: {
