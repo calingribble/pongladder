6db3f365523d298618d24643ad69c33274786c9c
Calin Gribble <calingribble@gmail.com>
2014-09-20 16:08:34 -0400

    init commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sarpgApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sarpgApp
+ */
+angular.module('sarpgApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
