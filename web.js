11e23fd69e4ba40f7294a98ba822a2fc600274ac
Calin Gribble <calingribble@gmail.com>
2014-12-22 14:47:09 -0500

    cp3

--- web.js
+++ web.js
@@ -4,5 +4,5 @@ var morgan = require('morgan');
 var app = express();
 
 app.use(morgan('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use(gzippo.staticGzip("" + __dirname + "/app"));
 app.listen(process.env.PORT || 5000);
